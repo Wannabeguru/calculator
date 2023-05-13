407f716c354ddb4bec1eaaba5f9737413df741d7
Wannabeguru <wannabegurux@gmail.com>
2023-05-13 13:53:40 -0700

    added functionality

--- javascript.js
+++ javascript.js
@@ -1,6 +1,23 @@
-const button = document.querySelectorAll('button')
 function appendToResult(text) {
     const result = document.getElementById('result');
     result.value += text;
-    
 }
+
+function getData() {
+    result = document.getElementById('result');
+    inputValue = result.value;
+    return inputValue;
+}
+
+function parseData() {
+    toParse = getData().split(/(\+|\-|\*|\/)/);
+    console.log(toParse);
+
+}
+
+//function operate() {
+   
+    
+//}
+
+
